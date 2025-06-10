9e882d738a4c18b1ef80e8dccb47cd71f75e9aa8
freddyvasquezstone <fvasquez@stone.com.co>
2025-06-10 07:22:10 -0500

    Creación de Vehiculos

--- src/data/processData.ts
+++ src/data/processData.ts
@@ -129,6 +129,7 @@ export const processData: Phase[] = [
         pdfLink: 'https://drive.google.com/file/d/19iJTZI_c3j0huuWF0HU9VcQi_Ku_SBhi/view?usp=drive_link',
         figmaLink: 'https://www.figma.com/board/FexMN0ZtIh1Cp4CbV5jIOW/Creacion-de-Vehiculo?node-id=0-1&p=f&t=33efp8dlAvYMyk0w-0'
       },
+      
       {
         id: 'activacion-vehiculo',
         name: 'ACTIVACIÓN VEHÍCULO',
