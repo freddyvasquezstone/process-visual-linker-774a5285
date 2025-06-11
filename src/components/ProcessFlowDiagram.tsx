f6211b15e41f5f345ae92704e55680a6b4a98b44
freddyvasquezstone <fvasquez@stone.com.co>
2025-06-11 16:34:36 -0500

    sumary1

--- src/components/ProcessFlowDiagram.tsx
+++ src/components/ProcessFlowDiagram.tsx
@@ -110,7 +110,6 @@ const ProcessFlowDiagram = () => {
             ))}
             
             <Legend />
-            <Summary />
           </TabsContent>
           
           <TabsContent value="excluded-processes">
@@ -118,7 +117,7 @@ const ProcessFlowDiagram = () => {
           </TabsContent>
         </Tabs>
 
-        
+        <Summary />
       </div>
     </div>
   );
