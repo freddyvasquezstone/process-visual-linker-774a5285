3c889f2eca47e54b966aaed97f608505400b1038
freddyvasquezstone <fvasquez@stone.com.co>
2025-06-12 16:08:08 -0500

    Operacion Internacional

--- src/data/processData.ts
+++ src/data/processData.ts
@@ -157,10 +157,12 @@ export const processData: Phase[] = [
         pdfLink: 'https://drive.google.com/file/d/1RuSUDwQO9_AFC9c0Bjx_ceL4ZHUxRPLp/view?usp=drive_link',
         figmaLink: 'https://www.figma.com/board/2PvvDR3GIsmpDbqJ8LnkeQ/Reexpedici%C3%B3n-de-Manifiestos?node-id=0-1&p=f&t=sbPa2ET0OpTbNfIr-0'
       },
-      
+
       {
         id: 'operacion-internacional',
-        name: 'OPERACIÓN INTERNACIONAL'
+        name: 'OPERACIÓN INTERNACIONAL',
+        pdfLink: '', 
+        figmaLink: 'https://www.figma.com/board/qiGlYwHKuJPRhhZXTfdaNE/Operacion-Internacional-HUMADEA?node-id=0-1&p=f&t=fkwyPiIgBjRNmFbA-0'
       },
       {
         id: 'comercio-exterior',
