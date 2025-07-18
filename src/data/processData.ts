0bfc1aa6e81a72f7ef1ec928ab3b74b4ffc2870c
freddyvasquezstone <fvasquez@stone.com.co>
2025-07-18 10:48:32 -0500

    refinado activacion vehiculo

--- src/data/processData.ts
+++ src/data/processData.ts
@@ -161,8 +161,8 @@ export const processData: Phase[] = [
         name: 'ACTIVACIÓN DE VEHÍCULO',
         pdfLink: 'https://drive.google.com/file/d/1UpJLJ0v8r79PbSivKvfHdqmR_dCTCRKw/view?usp=drive_link',
         figmaLink: 'https://www.figma.com/board/hQeE8dzKmGoItzKi2Vutaw/An%C3%A1lisis-y-Seguridad?node-id=0-1&p=f&t=Hpy9SuGFySKbMYxk-0',
-        //documentoRefinadoLink: '',
-        //documentoObservacionesLink: '',
+        documentoRefinadoLink: 'https://drive.google.com/file/d/1IldyYlcltHgrjWykirOsqHNNe1BQHBXH/view?usp=drive_link ',
+        documentoObservacionesLink: 'https://docs.google.com/spreadsheets/d/1bbWHv625FcWWOSPASW0AFITocF6xw4bt/edit?usp=drive_link&ouid=115478075634609640734&rtpof=true&sd=true',
         responsableStatus: 'validacion-humadea'
       },
       {
