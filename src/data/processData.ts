43a7e0d13970b8569cd7f662574965fb1b9f4161
freddyvasquezstone <fvasquez@stone.com.co>
2025-08-15 12:54:14 -0500

    Vehiculos

--- src/data/processData.ts
+++ src/data/processData.ts
@@ -152,8 +152,8 @@ export const processData: Phase[] = [
         name: 'CREACIÓN DE VEHÍCULO',
         pdfLink: 'https://drive.google.com/file/d/19iJTZI_c3j0huuWF0HU9VcQi_Ku_SBhi/view?usp=drive_link',
         figmaLink: 'https://www.figma.com/board/FexMN0ZtIh1Cp4CbV5jIOW/Creacion-de-Vehiculo?node-id=0-1&p=f&t=33efp8dlAvYMyk0w-0',
-        documentoRefinadoLink: 'https://drive.google.com/file/d/1dexgePZ8Ch2J8lB_euXMH5we1q7R539M/view?usp=drive_link',
-        //documentoObservacionesLink: '',
+        //documentoRefinadoLink: 'https://drive.google.com/file/d/1dexgePZ8Ch2J8lB_euXMH5we1q7R539M/view?usp=drive_link',
+        documentoObservacionesLink: 'https://docs.google.com/spreadsheets/d/1bVqlWRwl7nOm6HlhS2e4OzN-EMLl4NGe/edit?usp=drive_link&ouid=115478075634609640734&rtpof=true&sd=true',
         responsableStatus: 'validacion-stone'
       },
       {
